7948177f457f5620df1d85d45cdbb5d93ac44422
l-woody-l <cmh1970@outlook.com>
2020-01-25 14:51:01 -0500

    First Commit

--- Client.py
+++ Client.py
@@ -6,4 +6,8 @@ port = 3149
 
 s.connect((host, port))
 print(s.recv(1024))
+
+name = input("What's your name?")
+s.send(name.encode())
+
 s.close()
--- Server.py
+++ Server.py
@@ -6,10 +6,12 @@ port = 3149
 
 s.bind((host, port))
 s.listen(5)
-print((host, port))
+print("Listening")
 
 while True:
     c, addr = s.accept()
     print('got connection from', addr)
-    c.send(bytes("Message has been received", 'utf-8'))
+    c.send(bytes("Connection Established", 'utf-8'))
+    c, name = s.accept()
+    ##c.send()
     c.close()
