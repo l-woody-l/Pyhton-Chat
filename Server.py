b1696459793ac8d306602d1c37001461bc23815d
l-woody-l <cmh1970@outlook.com>
2020-01-25 16:01:06 -0500

    Added the communications in a function

--- Server.py
+++ Server.py
@@ -3,16 +3,19 @@ import socket
 s = socket.socket()
 host = 'localhost'
 port = 3149
-names = ""
 
 s.bind((host, port))
 s.listen(5)
 print("Listening")
 
-while True:
+def connection():
+    names = ""
     c, addr = s.accept()
     print('got connection from', addr)
     names += str(c.recv(1024), "utf-8")
     c.send(bytes("Connection Established. Recent Names: " + names, "utf-8"))
     print('Connection ended with', addr)
     c.close()
+
+while True:
+    connection()
